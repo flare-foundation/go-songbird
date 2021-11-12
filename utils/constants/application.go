1a30aaa9daaef489fa7910d1cd7a9ccb68093d40
Max Wolter <awfm9@protonmail.com>
2021-11-12 13:46:41 +0000

    Undo versioning changes

--- utils/constants/application.go
+++ utils/constants/application.go
@@ -6,8 +6,8 @@ package constants
 // Const variables to be exported
 const (
 	// PlatformName exports the name of the platform
-	PlatformName = "flare"
+	PlatformName = "avalanche"
 
 	// AppName exports the name of the avalanche application
-	AppName = "flarego"
+	AppName = "avalanchego"
 )
