a7ebe79a8007d58fa58d2a6a7007a31d2fdacc48
Max Wolter <awfm9@protonmail.com>
2021-11-27 12:28:17 +0000

    Check if bootstrapping without validators works

--- chains/manager.go
+++ chains/manager.go
@@ -388,7 +388,16 @@ func (m *manager) buildChain(chainParams ChainParameters, sb Subnet) (*chain, er
 	consensusParams.Namespace = fmt.Sprintf("%s_%s", constants.PlatformName, primaryAlias)
 
 	// The validators of this blockchain
-	vdrs := validators.FBA
+	var vdrs validators.Set // Validators validating this blockchain
+	var ok bool
+	if m.StakingEnabled {
+		vdrs, ok = m.Validators.GetValidators(chainParams.SubnetID)
+	} else { // Staking is disabled. Every peer validates every subnet.
+		vdrs, ok = m.Validators.GetValidators(constants.PrimaryNetworkID)
+	}
+	if !ok {
+		return nil, fmt.Errorf("couldn't get validator set of subnet with ID %s. The subnet may not exist", chainParams.SubnetID)
+	}
 
 	beacons := vdrs
 	if chainParams.CustomBeacons != nil {
