2dc13512078e70c83a171d0a1a304a19b0ec5343
Max Wolter <awfm9@protonmail.com>
2021-12-02 17:47:32 +0000

    Enable Apricot Phases 3 and 4

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,7 +7,7 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.1.8'}
+coreth_version=${CORETH_VERSION:-'51759a790da94e83025369b2b4fbf651654123f5'}
 
 # Changes to the minimum golang version must also be replicated in
 # README.md
--- version/constants.go
+++ version/constants.go
@@ -36,10 +36,13 @@ var (
 	ApricotPhase2Times       = map[uint32]time.Time{}
 	ApricotPhase2DefaultTime = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	ApricotPhase3Times       = map[uint32]time.Time{}
+	ApricotPhase3Times = map[uint32]time.Time{
+		constants.LocalID: time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)}
 	ApricotPhase3DefaultTime = time.Date(2100, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	ApricotPhase4Times       = map[uint32]time.Time{}
+	ApricotPhase4Times = map[uint32]time.Time{
+		constants.LocalID: time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+	}
 	ApricotPhase4DefaultTime = time.Date(2100, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 	ApricotPhase4MinPChainHeight        = map[uint32]uint64{}
