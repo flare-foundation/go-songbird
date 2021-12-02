d933f39854775206ece60475066bf512acede77c
Max Wolter <awfm9@protonmail.com>
2021-12-02 17:53:22 +0000

    Update Coreth to Apricot Phase 3 and 4 enabled

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,7 +7,7 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.1.8'}
+coreth_version=${CORETH_VERSION:-'438c75fa5bfb0f7a21d0deecb0922905ef3b6e7a'}
 
 # Changes to the minimum golang version must also be replicated in
 # README.md
