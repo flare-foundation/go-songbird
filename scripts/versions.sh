909489fe0f81c703adf1761bb346986418c00292
Max Wolter <awfm9@protonmail.com>
2022-03-17 20:12:24 +0000

    Update Coreth dependency

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.3.3'}
+coreth_version=${CORETH_VERSION:-'70776f7aa5a74e23bb95b7f7a7a33499c8e8ef6f'}
