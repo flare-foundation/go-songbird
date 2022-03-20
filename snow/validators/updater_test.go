41dad526e4c7713beb4984346f58d37da50e130a
Satyajit Das <emailtovamos@gmail.com>
2022-03-20 10:32:11 +0000

    Retriever updater unit test (#27)
    * Retriever updater unit test
    
    * Only assert package
    
    * Split file and check for return
    
    * Counter for Cache access check
    
    * Replace NewDefaultSet() after rebasing.
    
    * Formatting
    
    * Non nil error and cache check cases
    
    * Address most comments for retriever_test except cache count
    
    * Count how often underlying retriever is called + Decouple check for if correct blockID is passed
    
    * Updater tests to check for error case and check for underlying retriever
    
    * Address remaining comments

---
+++ snow/validators/updater_test.go
@@ -0,0 +1,61 @@
+package validators
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/flare-foundation/flare/ids"
+)
+
+func TestUpdaterFromDefaultSet(t *testing.T) {
+	testBlockID := ids.ID{
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+		0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+	}
+	costonSet := loadCostonValidators(t)
+
+	t.Run("nominal case", func(t *testing.T) {
+		t.Parallel()
+
+		callsCount := 0
+
+		retrieverMock := &TestRetriever{
+			GetValidatorsByBlockIDFunc: func(blockID ids.ID) (Set, error) {
+				callsCount++
+				return costonSet, nil
+			},
+		}
+		cr := NewCachingRetriever(retrieverMock)
+		u := NewUpdater(loadCostonValidators(t), cr)
+
+		err := u.UpdateValidators(testBlockID)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 1, callsCount)
+
+		set, err := cr.GetValidators(testBlockID)
+		assert.Equal(t, costonSet, set)
+		assert.Equal(t, 1, callsCount)
+	})
+
+	t.Run("handles failure to get validators", func(t *testing.T) {
+		t.Parallel()
+
+		retrieverMock := &TestRetriever{
+			GetValidatorsByBlockIDFunc: func(blockID ids.ID) (Set, error) {
+				return nil, fmt.Errorf("Couldn't get validators")
+			},
+		}
+		cr := NewCachingRetriever(retrieverMock)
+		u := NewUpdater(NewSet(), cr)
+
+		err := u.UpdateValidators(testBlockID)
+		require.Error(t, err)
+	})
+
+}
