464035f787ba416c1b5666c727ba6be91f19cde3
jp <jp@txt3.com>
2022-03-22 02:39:36 +0000

    mod build script to use grocksdb internal makefile.

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,6 +27,7 @@ else
   cd "$HOME/go/pkg/mod/github.com/xrpdevs/$GROCKS_DIR/"
   echo "Building rocksdb from source, this could take some time. You must be able to use sudo to install rocksdb."
   echo "NOTE: Building RocksDB is memory intensive, please make sure you have at least 16GB RAM (or RAM + Swap)"
+
   make -j 16
 fi
 
