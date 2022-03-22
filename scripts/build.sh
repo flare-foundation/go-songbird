758732099f0c2a9843266054a6de761fde06b4de
jp <jp@txt3.com>
2022-03-22 04:46:56 +0000

    fix directory permission in grocksdb golang module dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,21 +19,24 @@ go mod download
 
 # only use the latest rocksdb in the /xrpdevs/ namespace in the go package cache
 
-GROCKS_DIR="$(ls -t $HOME/go/pkg/mod/github.com/xrpdevs/ | grep grocksdb | head -1)"
+GROCKS_DIR="$HOME/go/pkg/mod/github.com/xrpdevs/$(ls -t $HOME/go/pkg/mod/github.com/xrpdevs/ | grep grocksdb | head -1)"
 
 if [ -z ${ROCKSDBALLOWED+x} ]; then
   echo "Building for LevelDB"
 else
-  cd "$HOME/go/pkg/mod/github.com/xrpdevs/$GROCKS_DIR/"
+  cd "$GROCKS_DIR"
   echo "Building rocksdb from source, this could take some time. You must be able to use sudo to install rocksdb."
   echo "NOTE: Building RocksDB is memory intensive, please make sure you have at least 16GB RAM (or RAM + Swap)"
   chmod 0777 .
   mv build.sh _build.sh
   cat _build.sh | sed 's/sudo//g' >build.sh
   make -j 16
+  export CGO_CFLAGS="-I$GROCKS_DIR/dist/include"
+  export CGO_LDFLAGS="-L$GROCKS_DIR/dist/lib -lrocksdb -lstdc++ -lm -lz -lsnappy -llz4 -lzstd"
 fi
 
 cd "$FLARE_PATH"
+
 
 
 # Build flare
